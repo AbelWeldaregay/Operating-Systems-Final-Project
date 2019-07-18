0a6d5b056410936f6fb9084be7ed59c32f976533
Abel Weldaregay <abelweldaregay@gmail.com>
2019-07-17 20:27:31 -0400

    add interface for shared memory

--- prodcons/src/com/cs471/prodcons/Buffer.java
+++ prodcons/src/com/cs471/prodcons/Buffer.java
@@ -1,5 +1,19 @@
 package com.cs471.prodcons;
 
-public class Buffer {
-
+/**
+ * Interface for shared memory.
+ * @author Abel Weldaregay
+ */
+public interface Buffer <E>
+{
+	/**
+	 * @description Producers call this method
+	 * @param item
+	 */
+	public void insert(E item);
+	/**
+	 * @description Consumers call this method
+	 * @return
+	 */
+	public E remove();
 }
