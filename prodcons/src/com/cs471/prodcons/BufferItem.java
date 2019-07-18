57921d2faed28f768dcb894cf47e0f58057fa3a5
Abel Weldaregay <abelweldaregay@gmail.com>
2019-07-17 20:11:15 -0400

    add implementation for BufferItem.java

--- prodcons/src/com/cs471/prodcons/BufferItem.java
+++ prodcons/src/com/cs471/prodcons/BufferItem.java
@@ -1,5 +1,59 @@
 package com.cs471.prodcons;
 
-public class BufferItem {
+/**
+ * @description represents a buffer item
+ */
+class BufferItem {
+    /**
+     * @description holds sales date
+     */
+    private String salesDate;
+    /**
+     * @description store id
+     */
+    private int storeId;
+    /**
+     * @description register id
+     */
+    private int registerId;
+    /**
+     * @description sale amount
+     */
+    private float saleAmount;
+
+    public BufferItem() {
+        this.salesDate = "EMPTY";
+        this.storeId = 0;
+        this.registerId = 0;
+        this.saleAmount = 0;
+    }
+
+    public void setSaleAmount(float rhs) {
+        this.saleAmount = rhs;
+    }
+    public void setRegisterId(int rhs) {
+        this.registerId = rhs;
+    }
+    public void setStoreId(int rhs) {
+        this.storeId = rhs;
+    }
+    public void setSalesDate(String rhs) {
+        this.salesDate = rhs;
+    }
+    public String getSalesDate() {
+        return this.salesDate;
+    }
+
+    public int getStoreId() {
+        return this.storeId;
+    }
+
+    public int getRegisterId() {
+        return this.registerId;
+    }
+
+    public float getSaleAmount() {
+        return this.saleAmount;
+    }
 
 }
