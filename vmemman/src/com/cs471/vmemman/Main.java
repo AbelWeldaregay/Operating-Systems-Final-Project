4e688e0424d74d7e813f7891585adf02f51a2c6a
Abel Weldaregay <abelweldaregay@gmail.com>
2019-07-30 22:06:12 -0400

    add gradle build for second project '

--- vmemman/src/com/cs471/vmemman/Main.java
+++ vmemman/src/com/cs471/vmemman/Main.java
@@ -1,7 +1,7 @@
 package com.cs471.vmemman;
 
 public class Main {
-public static void main(String args[]) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+	
+	}
 }
