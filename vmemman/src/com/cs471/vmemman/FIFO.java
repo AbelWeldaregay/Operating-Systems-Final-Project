243c7a8335ac57a04606662a7a1fcc29ef27557f
Abel Weldaregay <abelweldaregay@gmail.com>
2019-08-03 21:30:44 -0400

    add doc for FIFO algorithm

--- vmemman/src/com/cs471/vmemman/FIFO.java
+++ vmemman/src/com/cs471/vmemman/FIFO.java
@@ -5,22 +5,44 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-
+/**
+ * Represents a First In First Out Algorithm
+ * @author Abel Weldaregay
+ *
+ */
 public class FIFO {
-
+	/**
+	 * Holds the pages in memory, in first in first out order
+	 */
 	static Queue<Integer> inMemoryPages = new LinkedList<Integer>();
+	/**
+	 * Holds the frame size
+	 */
 	private int frameSize;
+	/**
+	 * Holds the pages
+	 */
 	private ArrayList<Integer> pages = new ArrayList<Integer>();
 	
+	/**
+	 * Creates a FIFO instance with given
+	 * frameSize and pages
+	 * @param frameSize
+	 * @param pages
+	 */
 	FIFO(int frameSize, ArrayList<Integer> pages) {
 		this.frameSize = frameSize;
 		this.pages = pages;
 	}
 
-	
+	/**
+	 * Performs FIFO algorithm with the
+	 * given page frame size and pages
+	 * @return page fault in %
+	 */
 	public String FIFOPageReplacmentAlgo() {
 		
-		HashSet<Integer> s = new HashSet<>();
+		HashSet<Integer> frameSet = new HashSet<>(this.frameSize);
 		
 		int pageFaults = 0;
 		int hits = 0;
@@ -28,13 +50,13 @@ public class FIFO {
 		Queue<Integer> inMemoryPages = new LinkedList<Integer>();
 		for (int i = 0; i < numberOfPages; i++) {
 			// check if the current frame has space
-			if (s.size() < frameSize) {
+			if (frameSet.size() < frameSize) {
 				/*
 				 *Insert it into set if not present
 				 *already which represents page fault 
 				 */
-				if (!s.contains(pages.get(i))) {
-					s.add(pages.get(i));
+				if (!frameSet.contains(pages.get(i))) {
+					frameSet.add(pages.get(i));
 //					System.out.println("PAGE FAULT AT PAGE #: " + this.pages.get(i));
 					pageFaults++;
 					/*
@@ -50,18 +72,18 @@ public class FIFO {
 			 */
 			else {
 				
-				if (!s.contains(pages.get(i))) {
+				if (!frameSet.contains(pages.get(i))) {
 					//System.out.println("PAGE FAULT AT PAGE #: " + this.pages.get(i));
 					int firstPage = inMemoryPages.peek();
 					inMemoryPages.poll();
 					/*
 					 * Remove it from the frame
 					 */
-					s.remove(firstPage);
+					frameSet.remove(firstPage);
 					/*
 					 *Insert the current page 
 					 */
-					s.add(pages.get(i));
+					frameSet.add(pages.get(i));
 					/*
 					 * Insert the current page to in memory pages
 					 */
@@ -70,6 +92,10 @@ public class FIFO {
 				}
 			}
 		}
+		/*
+		 * Calculating the percentage of page faults and
+		 * returning the percentage of page faults
+		 */
 		DecimalFormat df2 = new DecimalFormat("0.00");
 		float percentage = ((float) pageFaults) / numberOfPages;
 		percentage = percentage * 100;
