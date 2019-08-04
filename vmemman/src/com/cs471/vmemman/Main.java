b9a8500c800b56b637d0fd1c3077c9981c05245d
Abel Weldaregay <abelweldaregay@gmail.com>
2019-08-03 21:12:56 -0400

    add api doc for Main.java

--- vmemman/src/com/cs471/vmemman/Main.java
+++ vmemman/src/com/cs471/vmemman/Main.java
@@ -12,21 +12,42 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	
+	/**
+	 * Holds the virtual addresses, right after 
+	 * being read from the file, These are not converted
+	 * to page numbers yet
+	 */
 	public static ArrayList<Integer> virtualAddresses = new ArrayList<Integer>();
+	/**
+	 * Holds the given page sizes from requirements specification doc
+	 */
 	public static int[] pageSizes = {512, 1024, 2048};
+	/**
+	 * Holds the frames from the requirements specification doc
+	 */
 	public static int[] frames = {4, 8, 12};
+	/**
+	 * Holds the algorithms to be conducted
+	 */
 	public static String[] algorithms = {"FIFO", "LRU", "MRU", "OPT"};
+	/**
+	 * Holds the pages, calculated by using
+	 * the formula virtualAddress / page size = page #
+	 */
 	public static ArrayList<Integer> pages = new ArrayList<>();
 	
-	
+	/**
+	 * Driver method
+	 * @param args
+	 * @throws FileNotFoundException
+	 */
 	public static void main(String[] args) throws FileNotFoundException {
+		/*
+		 * Reading the given input file ./src/com/cs471/vmemman/inputfile.txt
+		 * The .dat file has been converted to .txt to make it easier
+		 */
 		readInputFile();
-		
-		for (int i = 0; i < virtualAddresses.size(); i++) {
-			pages.add(virtualAddresses.get(i)/512);
-		}
-		
+
 		System.out.println(" _______________________________________________________");
 		System.out.println("| Page Size | # of Pages | Algorithm | % of Page Faults |");
 		System.out.println("");
@@ -72,6 +93,13 @@ public class Main {
 		
 	}
 	
+	/**
+	 * Reads the virtual addresses from
+	 * given input file, the input file has
+	 * been converted from a .dat file to a .txt
+	 * to make it easier to read
+	 * @throws FileNotFoundException
+	 */
 	public static void readInputFile() throws FileNotFoundException {
 		File file = new File("./src/com/cs471/vmemman/inputfile.txt"); 
 		Scanner sc = new Scanner(file);
